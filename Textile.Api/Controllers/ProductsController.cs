f9e59d58cca4ca2a127916bae54c7f65e7b7a9a8
MD CHAND <cmd78692@outlook.com>
2026-09-21 13:30:53 +0530

    Add project files.

---
+++ Textile.Api/Controllers/ProductsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Textile.Api.Data;
+using Textile.Api.Models;
+
+namespace Textile.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+        // GET: api/Products/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Products.Update(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        // DELETE: api/Products/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Textile.Api/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Textile.Api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
